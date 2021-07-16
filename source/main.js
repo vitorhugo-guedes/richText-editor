7158d585416dd7f02aa8295f87ceed86a672bbdf
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-07-16 17:00:13 -0300

    update

--- source/main.js
+++ source/main.js
@@ -1,23 +1,29 @@
 const editor = document.getElementById('editor');
 const tools = document.querySelectorAll('.option');
+const selectFontSize = document.getElementById('fontSize');
 
 function load(){
-    window.document.designMode = 'on';
+    editor.document.designMode = 'On';
 }
+
+selectFontSize.addEventListener('change', ()=>{
+    let option = selectFontSize.options[selectFontSize.selectedIndex];
+    applyCommand('fontSize', option.value);
+})
 
 tools.forEach(btn => {
     let cmd = btn.dataset.command;
     btn.addEventListener('click', ()=>{
-        applyCommand(cmd);
+        applyCommand(cmd, null);
     })
 });
 
-function applyCommand(command){
+function applyCommand(command, value){
     if(command === 'insertImage' || command === 'createLink'){
-        let value = prompt('insert here:', 'http://');
-        document.execCommand(command, false, value);
+        let link = prompt('insert here:', 'http://');
+        document.execCommand(command, false, link);
     }else{
-        document.execCommand(command, false, null);
+        document.execCommand(command, false, value);
     }
     
 }
