e3957f36457ae24ccdf7d05d8c46c82c4df01629
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-07-16 19:49:27 -0300

    Update
    - Font formatatting included

--- source/main.js
+++ source/main.js
@@ -1,15 +1,35 @@
-const editor = document.getElementById('editor');
-const tools = document.querySelectorAll('.option');
-const selectFontSize = document.getElementById('fontSize');
+//criar um wrapper para os selects e usar o foreach para reduzir as linhas de código e eventos necessários. adicionar window.getSelection().empty();
+//Verificar se o comando heading é suportado, se não executar o comando formatBlock.
 
 function load(){
     editor.document.designMode = 'On';
 }
 
+const editor = document.getElementById('editor');
+const tools = document.querySelectorAll('.option');
+
+const selectFontSize = document.getElementById('fontSize');
+const selectFontTitles = document.getElementById('fontTitles');
+const selectFontFamilies = document.getElementById('fontFamilies');
+
+
 selectFontSize.addEventListener('change', ()=>{
-    let option = selectFontSize.options[selectFontSize.selectedIndex];
-    applyCommand('fontSize', option.value);
+    let fontSizeOption = selectFontSize.options[selectFontSize.selectedIndex];
+    applyCommand('fontSize', fontSizeOption.value);
 })
+
+selectFontTitles.addEventListener('change', ()=>{
+    let fontTitleOption = selectFontTitles.options[selectFontTitles.selectedIndex];
+    applyCommand('formatBlock', fontTitleOption.value);
+    
+})
+
+selectFontFamilies.addEventListener('change', () =>{
+    let fontFamilyOption = selectFontFamilies.options[selectFontFamilies.selectedIndex];
+    applyCommand('fontname', fontFamilyOption.value);
+    
+})
+
 
 tools.forEach(btn => {
     let cmd = btn.dataset.command;
